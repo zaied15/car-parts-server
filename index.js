0e3564044f7274fbf42e5d0894c76ee7f326b99a
unknown <mda800@gmail.com>
2022-05-23 08:30:14 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// MongoDB connection
+const uri = `mongodb+srv://${process.env.DB_ADMIN}:${process.env.DB_PASS}@cluster0.nka8o.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 async function run() {
   try {
+    await client.connect();
+
+    // Database Collection
+    const partsCollection = client.db("pitsTop").collection("parts");
+
+    // Login authentication
   } finally {
   }
 }
