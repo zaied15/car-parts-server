e80a29cc98d4584e5e4e79af3cad368db008cfbf
unknown <mda800@gmail.com>
2022-05-29 18:37:25 +0600

    reverse test

--- index.js
+++ index.js
@@ -8,24 +8,24 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // Middleware
-// app.use(cors());
-// app.use(express.json());
-const corsConfig = {
-  origin: "*",
-  credentials: true,
-  methods: ["Get", "POST", "PUT", "DELETE"],
-};
-app.use(cors(corsConfig));
-app.options("*", cors(corsConfig));
+app.use(cors());
 app.use(express.json());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With,Content-Type,Accept,authorization"
-  );
-  next();
-});
+// const corsConfig = {
+//   origin: "*",
+//   credentials: true,
+//   methods: ["Get", "POST", "PUT", "DELETE"],
+// };
+// app.use(cors(corsConfig));
+// app.options("*", cors(corsConfig));
+// app.use(express.json());
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With,Content-Type,Accept,authorization"
+//   );
+//   next();
+// });
 
 // Verify JWT Token
 function verifyJwt(req, res, next) {
